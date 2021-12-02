e86cf750a378aa5df62e8175de13561309c32fa3
thomaspara <thomasjohnparashos@gmail.com>
2021-12-02 11:11:36 -0800

    script changes

--- DiscordBot.sh
+++ DiscordBot.sh
@@ -1,3 +1,9 @@
+while [ -z "$(fping google.com | grep alive)" ]
+do
+    echo "waiting for internet ..."
+    sleep 3
+done
+echo "Internet is now online"
 script_full_path=$(dirname "$(realpath "$0")")
 cd $script_full_path
 git checkout .

da359e34620b7e87aa3df390a5fe6bff3ba478d1
thomaspara <thomasjohnparashos@gmail.com>
2021-12-02 10:55:54 -0800

    adds reboot, stopping, bot channel, and stuff

--- discord_bot.py
+++ discord_bot.py
@@ -1,26 +1,34 @@
 import discord
 import secrets
 from process_manager import server_manager 
+from power_manager import power_manager
 print("bot started")
 
 def remove_prefix(text, prefix):
     return text[text.startswith(prefix) and len(prefix):]
 
 client = discord.Client()
-summon="/cock"
+summon="/cock "
 srv_num = 0 #todo implement this
 
+power = power_manager()
 mc_v_server = server_manager("mcv", secrets.vanilla_mc_path)
 ter_v_server = server_manager("terv", secrets.vanilla_ter_path)
 
 help_msg = f'''to use me say `{summon}` 
+I will ignore you if you have the Bot Banned role
 Commands:
 `minecraft start` starts minecraft server
-`minecraft send <server command>` sends <server command> to server
+`minecraft stop` stops minecraft server
+`minecraft send <server command>` sends <server command> to server, requires Minecraft Admin role
 `minecraft live` says if the server is live
+
 `terraria start` starts terraria server
-`terraria send <server command>` sends <server command> to server
+`terraria stop` stops terraria server
+`terraria send <server command>` sends <server command> to server, requires Minecraft Admin role
 `terraria live` says if the server is live
+
+`live` shows status of all servers
 `ip` shows ip list
 `help` shows this message'''
 
@@ -32,51 +40,86 @@ Ask Thomas for help if you can't connect
 
 @client.event
 async def on_message(message):
+    if message.channel.name != secrets.bot_channel : return
     if message.author == client.user: return
     if not message.content.startswith(summon) : return 
     roles = [r.name for r in message.author.roles]
+    if "Bot Banned" in roles: return
     msg = message.content
-    msg = msg.lower()
+    msg = msg.lower().strip()
 
-    if summon == msg or f'{summon} help' == msg:
+    if summon.strip() == msg or f'{summon}help' == msg:
         rsp = help_msg
     #vanilla minecraft
-    elif msg == f'{summon} minecraft start':
+    elif msg == f'{summon}minecraft start':
         if mc_v_server.start():
-            rsp = "starting server"
+            rsp = f"starting server, be sure to run `{summon}minecraft stop` when you are done"
         else:
             rsp = "server is running"
-    elif msg == f'{summon} minecraft live':
+    elif msg == f'{summon}minecraft stop':
+        if mc_v_server.send('stop'):
+            rsp = "stopping minecraft"
+        else:
+            rsp = "server already offline"
+    elif msg == f'{summon}minecraft live':
         if mc_v_server.is_running():
             rsp = "server is running"
         else:
             rsp = "server is offline"
-    elif f'{summon} minecraft send ' in msg and "Minecraft Admin" in roles:
+    elif f'{summon}minecraft send ' in msg and "Minecraft Admin" in roles:
         cmd = remove_prefix(msg,f'{summon} minecraft send ')
         if mc_v_server.send(cmd):
             rsp = f"sent `{cmd}`"
         else:
             rsp = "server is offline"
     #vanilla terraria
-    elif msg == f'{summon} terraria start':
+    elif msg == f'{summon}terraria start':
         if ter_v_server.start():
-            rsp = "starting server"
+            rsp = f"starting server, be sure to run `{summon}terraria stop` when you are done"
         else:
             rsp = "server is running"
-    elif msg == f'{summon} terraria live':
+    elif msg == f'{summon}terraria stop':
+        if ter_v_server.send('exit'):
+            rsp = "stopping terraria"
+        else:
+            rsp = "server already offline"
+    elif msg == f'{summon}terraria live':
         if ter_v_server.is_running():
             rsp = "server is running"
         else:
             rsp = "server is offline"
-    elif f'{summon} terraria send ' in msg and "Terraria Admin" in roles:
+    elif f'{summon}terraria send ' in msg and "Terraria Admin" in roles:
         cmd = remove_prefix(msg,f'{summon} terraria send ')
         if ter_v_server.send(cmd):
             rsp = f"sent `{cmd}`"
         else:
             rsp = "server is offline"
+    #power management
+    elif msg == f'{summon}reboot' and "Super Admin" in roles:
+        power.reboot()
+        rsp = "server rebooting"
+    #Super Admin tools
+    elif msg == f'{summon}stop all' and "Super Admin" in roles:
+        rsp = ""
+        if ter_v_server.is_running():
+            ter_v_server.send("exit")
+            rsp += "stopping terraria\n"
+        if mc_v_server.is_running():
+            mc_v_server.send("stop")
+            rsp += "stopping minecraft\n"
+        if rsp == "":
+            rsp = "nothing is running"
     #other stuff
-    elif msg == f'{summon} ip':
+    elif msg == f'{summon}ip':
         rsp = ips
+    elif msg == f'{summon}live':
+        rsp = ""
+        if ter_v_server.is_running():
+            rsp += "terraria is running\n"
+        if mc_v_server.is_running():
+            rsp += "minecraft is running\n"
+        if rsp == "":
+            rsp = "nothing is running"
     else:
         rsp = "unknown command"
 
--- power_manager.py
+++ power_manager.py
@@ -5,20 +5,24 @@ from subprocess import run, PIPE
 
 class power_manager:
 
-    def __init__(self,screen_name,cmd, exit_word):
-        self.screen_name = screen_name
-        self.cmd = cmd
-        self.exit_word = exit_word
+    #implement when I have wake on lan
+    # def __init__(self):
+    #     self.screen_name = screen_name
+    #     self.cmd = cmd
+    #     self.exit_word = exit_word
 
-    def _shutdown_helper(self, timer):
-        t = timer if timer > 0 else 0
-        os.system(f"shutdown +{t+5}")
-        time.sleep(t*60)
-        self.send(self.exit_word)
+    # def _shutdown_helper(self, timer):
+    #     t = timer if timer > 0 else 0
+    #     os.system(f"shutdown +{t+5}")
+    #     time.sleep(t*60)
+    #     self.send(self.exit_word)
 
-    def shutdown(self, timer):
-        if timer == "stop": 
-            os.system("shutdown -c")
-        p = Process(target = self._shutdown_helper, args = (int(timer),))
-        p.start()
-        return f"shutdown in {timer} min"
+    # def shutdown(self, timer):
+    #     if timer == "stop": 
+    #         os.system("shutdown -c")
+    #     p = Process( target = self._shutdown_helper, args = (int(timer),) )
+    #     p.start()
+    #     return f"shutdown in {timer} min"
+
+    def reboot(self):
+        os.system("shutdown -r +1")
